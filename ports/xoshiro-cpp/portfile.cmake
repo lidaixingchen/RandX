a03a200bc882359a47f2458a0b068514c3e5097e
lidaixingchen <3506962253@qq.com>
2026-07-21 05:56:38 +0800

    chore: 更新 vcpkg portfile SHA512（v2.0.0 tag）

--- ports/xoshiro-cpp/portfile.cmake
+++ ports/xoshiro-cpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lidaixingchen/Pseudo-random-number-generator-based-on-Xoshiro
     REF v${VERSION}
-    SHA512 0  # 发布 tag 后需更新
+    SHA512 260dff775e6d70fdc69fd805314c143c18bf70675c38f18f80bd3c247bb2e6b4500051d1c9000db4f0db0cea260d5a24bfd9bec06fcc3a268d117237dfcf848f
     HEAD_REF master
 )
 
